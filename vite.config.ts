a594fa44eac2cf3f0115b5d697996bfdc3367a03
Dmitry <dmitry@smilecdr.com>
2025-07-20 15:36:44 -0600

    ui of home page displays resumes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    devtoolsJson({ uuid: "b025c748-5fa1-407e-889f-d7dccb6635d5" }),
+  ],
 });
